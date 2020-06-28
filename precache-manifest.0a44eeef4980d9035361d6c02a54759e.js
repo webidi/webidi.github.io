30a159e9afb56819c556631f21ddfcbf7b91165e
webidi <67202004+webidi@users.noreply.github.com>
2020-06-28 19:35:24 +0200

    Add files via upload

---
+++ precache-manifest.0a44eeef4980d9035361d6c02a54759e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8380415577f4c5a5a1c94675cfe508fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c43422d33583edbca4cb",
+    "url": "/static/css/2.9be4f162.chunk.css"
+  },
+  {
+    "revision": "625e17d00b7dfa5eb116",
+    "url": "/static/css/main.a08363fb.chunk.css"
+  },
+  {
+    "revision": "c43422d33583edbca4cb",
+    "url": "/static/js/2.835f3fda.chunk.js"
+  },
+  {
+    "revision": "2d3bca580836d79fb6e81acee99ac3cc",
+    "url": "/static/js/2.835f3fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625e17d00b7dfa5eb116",
+    "url": "/static/js/main.82843e71.chunk.js"
+  },
+  {
+    "revision": "133e9f15021b5a71fae5",
+    "url": "/static/js/runtime-main.7a558010.js"
+  },
+  {
+    "revision": "104fd585f8d67af7b5f982725bc8a3d4",
+    "url": "/static/media/caja1.104fd585.jpg"
+  },
+  {
+    "revision": "ead7bf76be12e99e7f48855fc235fd75",
+    "url": "/static/media/caja2.ead7bf76.jpg"
+  },
+  {
+    "revision": "fb037854f6ee57d96690b1b0e9c35f5c",
+    "url": "/static/media/caja3.fb037854.jpg"
+  },
+  {
+    "revision": "728d0862f4cb24de499513fb6efc97c1",
+    "url": "/static/media/caja4.728d0862.jpg"
+  },
+  {
+    "revision": "c481c8f4e914de5522b963181a9cc32e",
+    "url": "/static/media/caja5.c481c8f4.jpg"
+  },
+  {
+    "revision": "0110daba4816137cf4ec006524e85875",
+    "url": "/static/media/logo.0110daba.svg"
+  },
+  {
+    "revision": "a964e423fe7a5f67603bc9679f1d529d",
+    "url": "/static/media/video1.a964e423.mp4"
+  },
+  {
+    "revision": "d13aafbb3133f545b386737d4bc21d27",
+    "url": "/static/media/video2.d13aafbb.mp4"
+  },
+  {
+    "revision": "9570adf0c216641895a5a224709daa90",
+    "url": "/static/media/video3.9570adf0.mp4"
+  }
+]);
